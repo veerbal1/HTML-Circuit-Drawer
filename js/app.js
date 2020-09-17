c67841f65fb3b4e4e9332e05bcae30b456701d8f
veerbal1 <veerbalsingh1@gmail.com>
2020-09-17 13:07:39 +0530

    Added JSON functionality

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
+var jsonData = [];
+let serialNumber = 1;
+let oldX;
+let oldY;
+let color = "red";
+let size = 2;
+const CANVAS_WIDTH_HEIGHT = 512;
+
 window.onload = () => {
-  let oldX;
-  let oldY;
   var stage = new createjs.Stage("demoCanvas");
   shape = new createjs.Shape();
-  color = "red";
-  size = 2;
-  const CANVAS_WIDTH_HEIGHT = 512;
   stage.canvas.width = CANVAS_WIDTH_HEIGHT;
   stage.canvas.height = CANVAS_WIDTH_HEIGHT;
   stage.canvas.style.backgroundColor = "#000";
@@ -15,22 +18,34 @@ window.onload = () => {
     var line = new createjs.Shape();
     var circle = new createjs.Shape();
     circle.graphics.beginFill("red").drawCircle(0, 0, 2);
+
+    // Draw line
     line.graphics
       .beginStroke("red")
       .setStrokeStyle(1, "round")
       .moveTo(oldX, oldY)
       .lineTo(e.stageX, e.stageY);
-    // console.log(e.stageX);
 
+    // Draw Dot
     circle.x = e.stageX;
     circle.y = e.stageY;
-    circle.id = Math.random();
     oldX = e.stageX;
     oldY = e.stageY;
-    // console.log(e.stageX, e.stageY);
+    // Add info to json array
+    addDotInfo(serialNumber, circle.x, circle.y);
 
     stage.addChild(line);
     stage.addChild(circle);
     stage.update();
   });
+};
+
+const addDotInfo = (sid, xPos, YPos) => {
+  const dotPosition = {};
+  dotPosition.sNo = sid;
+  dotPosition.xPosition = xPos;
+  dotPosition.yPosition = YPos;
+  jsonData.push(dotPosition);
+  console.log(jsonData);
+  serialNumber++;
 };
