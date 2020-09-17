e0b71fe5e1feadfeeaf2acc2f320e203f4b25ac7
veerbal1 <veerbalsingh1@gmail.com>
2020-09-17 12:44:57 +0530

    Added dot fucntionality on click and draw line

--- js/app.js
+++ js/app.js
@@ -1,27 +1,36 @@
 window.onload = () => {
+  let oldX;
+  let oldY;
   var stage = new createjs.Stage("demoCanvas");
-  stage.canvas.width = window.innerWidth - 40;
-  stage.canvas.height = window.innerHeight - 40;
-  stage.canvas.style.backgroundColor = "#000"
+  shape = new createjs.Shape();
+  color = "red";
+  size = 2;
+  const CANVAS_WIDTH_HEIGHT = 512;
+  stage.canvas.width = CANVAS_WIDTH_HEIGHT;
+  stage.canvas.height = CANVAS_WIDTH_HEIGHT;
+  stage.canvas.style.backgroundColor = "#000";
   console.log(stage.canvas.style.backgroundColor);
-  
-  
-
 
   stage.on("stagemousedown", (e) => {
+    var line = new createjs.Shape();
     var circle = new createjs.Shape();
-    circle.graphics.beginFill("red").drawCircle(0, 0, 1);
+    circle.graphics.beginFill("red").drawCircle(0, 0, 2);
+    line.graphics
+      .beginStroke("red")
+      .setStrokeStyle(1, "round")
+      .moveTo(oldX, oldY)
+      .lineTo(e.stageX, e.stageY);
     // console.log(e.stageX);
-    
+
     circle.x = e.stageX;
     circle.y = e.stageY;
     circle.id = Math.random();
-    console.log(e.stageX,e.stageY);
-    
+    oldX = e.stageX;
+    oldY = e.stageY;
+    // console.log(e.stageX, e.stageY);
+
+    stage.addChild(line);
     stage.addChild(circle);
     stage.update();
   });
-  //   circle.on("click", function () {
-  //     alert("clicked");
-  //   });
 };
