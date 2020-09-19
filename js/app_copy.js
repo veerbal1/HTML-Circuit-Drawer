6bf0c0144b8504caf300bb651c3f72850350c1cf
veerbal1 <veerbalsingh1@gmail.com>
2020-09-19 21:24:31 +0530

    Added return function at stagemousedown

--- js/app_copy.js
+++ js/app_copy.js
@@ -22,81 +22,7 @@ window.onload = () => {
   stage.canvas.height = CANVAS_WIDTH_HEIGHT;
   stage.canvas.style.backgroundColor = "#000";
 
-  stage.on("stagemousedown", (e) => {
-    var line = new createjs.Shape();
-    var circle = new createjs.Shape();
-    var dragger = new createjs.Container();
-    circle.graphics.beginFill(color).drawCircle(0, 0, 2);
-    if (
-      !(Math.abs(initialPointData.initialPoint.xPosition - e.stageX) <= 3) &&
-      !(Math.abs(initialPointData.initialPoint.yPosition - e.stageY) <= 3) &&
-      !closedCycle
-    ) {
-      // Draw line
-      // dragger.x = e.stageX;
-      // dragger.y = e.stageY;
-      line.graphics
-        .beginStroke("#fff")
-        .setStrokeStyle(1, "round")
-        .moveTo(oldX, oldY)
-        .lineTo(e.stageX, e.stageY);
-        
-
-      // Draw Dot
-      circle.x = e.stageX;
-      circle.y = e.stageY;
-      oldX = e.stageX;
-      oldY = e.stageY;
-      console.log(circle);
-      
-    } else if (
-      Math.abs(initialPointData.initialPoint.xPosition - e.stageX) <= 3 &&
-      Math.abs(initialPointData.initialPoint.yPosition - e.stageY) <= 3
-    ) {
-      console.log(
-        Math.abs(initialPointData.initialPoint.xPosition - e.stageX),
-        Math.abs(initialPointData.initialPoint.yPosition - e.stageY)
-      );
-      // Draw line
-      line.graphics
-        .beginStroke("#fff")
-        .setStrokeStyle(1, "round")
-        .moveTo(oldX, oldY)
-        .lineTo(
-          initialPointData.initialPoint.xPosition,
-          initialPointData.initialPoint.yPosition
-        );
-
-      // Draw Dot
-      circle.x = initialPointData.initialPoint.xPosition;
-      circle.y = initialPointData.initialPoint.YPosition;
-      oldX = initialPointData.initialPoint.xPosition;
-      oldY = initialPointData.initialPoint.YPosition;
-      console.log("Locked");
-      closedCycle = true;
-    }
-
-    // Show message when a closed cycle made (Lock drawing more line with dot)
-    if (closedCycle) {
-      console.info(
-        "Have come to same point so drawing locked leaving a closed cycle"
-      );
-    }
-
-    // Add info to json array
-    addDotInfo(serialNumber, circle.x, circle.y);
-    color = "#FF4500";
-    dragger.addChild(line);
-    dragger.addChild(circle);
-    stage.addChild(dragger);
-    // stage.addChild(line);
-    // stage.addChild(circle);
-    stage.update();
-    // console.log(dragger);
-
-    // dragger.add
-    
-  });
+  stage.on("stagemousedown", stageMouseDownFunction(stage));
   
   downloadBtn.addEventListener("click", () => {
     const a = document.createElement("a");
@@ -123,14 +49,84 @@ const addDotInfo = (sid, xPos, YPos) => {
     dotPosition.yPosition = YPos;
     jsonData.push(dotPosition);
     serialNumber++;
-    console.log(jsonData);
+    // console.log(jsonData);
   }
 };
 
 const stageMouseDownFunction = (stage) =>{
-  return function(evt){
+  return function(e){
     if (!stageLocked) {
+      var line = new createjs.Shape();
+    var circle = new createjs.Shape();
+    var dragger = new createjs.Container();
+    circle.graphics.beginFill(color).drawCircle(0, 0, 2);
+    if (
+      !(Math.abs(initialPointData.initialPoint.xPosition - e.stageX) <= 3) &&
+      !(Math.abs(initialPointData.initialPoint.yPosition - e.stageY) <= 3) &&
+      !closedCycle
+    ) {
+      // Draw line
+      // dragger.x = e.stageX;
+      // dragger.y = e.stageY;
+      line.graphics
+        .beginStroke("#fff")
+        .setStrokeStyle(1, "round")
+        .moveTo(oldX, oldY)
+        .lineTo(e.stageX, e.stageY);
+        
+
+      // Draw Dot
+      circle.x = e.stageX;
+      circle.y = e.stageY;
+      oldX = e.stageX;
+      oldY = e.stageY;
       
+    } else if (
+      Math.abs(initialPointData.initialPoint.xPosition - e.stageX) <= 3 &&
+      Math.abs(initialPointData.initialPoint.yPosition - e.stageY) <= 3
+    ) {
+      // console.log(
+      //   Math.abs(initialPointData.initialPoint.xPosition - e.stageX),
+      //   Math.abs(initialPointData.initialPoint.yPosition - e.stageY)
+      // );
+      // Draw line
+      line.graphics
+        .beginStroke("#fff")
+        .setStrokeStyle(1, "round")
+        .moveTo(oldX, oldY)
+        .lineTo(
+          initialPointData.initialPoint.xPosition,
+          initialPointData.initialPoint.yPosition
+        );
+
+      // Draw Dot
+      circle.x = initialPointData.initialPoint.xPosition;
+      circle.y = initialPointData.initialPoint.YPosition;
+      oldX = initialPointData.initialPoint.xPosition;
+      oldY = initialPointData.initialPoint.YPosition;
+      console.info("Locked");
+      closedCycle = true;
+    }
+
+    // Show message when a closed cycle made (Lock drawing more line with dot)
+    if (closedCycle) {
+      console.info(
+        "Have come to same point so drawing locked leaving a closed cycle"
+      );
+    }
+
+    // Add info to json array
+    addDotInfo(serialNumber, circle.x, circle.y);
+    color = "#FF4500";
+    dragger.addChild(line);
+    dragger.addChild(circle);
+    stage.addChild(dragger);
+    // stage.addChild(line);
+    // stage.addChild(circle);
+    stage.update();
+    // console.log(dragger);
+
+    // dragger.add
     }
   }
 }
